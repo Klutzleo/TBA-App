65d1f7548f8f25a59ef6e01d3bb47401543b329f
Klutzleo <klutzleo@gmail.com>
2025-09-11 11:35:03 -0700

    Update app.py
    trying to fix railway still

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from flask import Flask, jsonify
-from routes.schemas import schemas_bp  # ✅ Correct import
+from schemas import schemas_bp  # ✅ Correct import
 
 app = Flask(__name__)  # Create the Flask app first
 app.register_blueprint(schemas_bp)    # Register your blueprint
