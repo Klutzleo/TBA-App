dbba5a00a19f513bf9a1109b9979a3cd8c1345ae
Klutzleo <klutzleo@gmail.com>
2025-09-22 12:38:34 -0700

    trying to fix roll

--- routes/roll.py
+++ routes/roll.py
@@ -3,18 +3,6 @@ from backend.roll_logic import resolve_skill_roll
 
 roll_bp = Blueprint("roll", __name__)
 
-@roll_bp.route("/roll/skill", methods=["POST"])
-def roll_skill():
-    data = request.get_json()
-
-    result = resolve_skill_roll(
-        actor=data["actor"],
-        difficulty_die=data.get("difficulty_die"),
-        bap_triggered=data.get("bap", False)
-    )
-
-    return jsonify(result)
-
 # Roll Skill
 @roll_bp.route("/roll/skill", methods=["POST"])
 def roll_skill():
