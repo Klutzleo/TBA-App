e4efb7761587d3ad7ade422e766225e77dd8c6b7
Klutzleo <klutzleo@gmail.com>
2025-09-23 08:59:53 -0700

    Update app.py
    forcing 0.0.0.0. 8080 for railway

--- backend/app.py
+++ backend/app.py
@@ -132,6 +132,6 @@ def metrics_route():
 
 # Local-only entry point for development
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 application = app
