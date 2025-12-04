bd3cf01eb10d075c044e0bfcea31f1d062477deb
Klutzleo <klutzleo@gmail.com>
2025-12-04 12:31:25 -0800

    Deleted old files not needed any more :(

--- routes/schemas/blp.py
+++
@@ -1,8 +0,0 @@
-from flask_smorest import Blueprint
-
-schemas_blp = Blueprint(
-    "Schemas",
-    "schemas",
-    url_prefix="/api/schemas",
-    description="Schema validation, emotional reflection, and playgrounds"
-)
